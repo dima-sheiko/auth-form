628a02f6cb2dc53f15c20caaf11a89890b6a1486
Дмитрий Шейко <sheydim@gmail.com>
2023-01-24 15:35:36 +0300

    Create store

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit/dist/configureStore';
+import userReducer from './userSlice';
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
