b7d08112790494523e65bd74b9f3961b5c798fd9
Дмитрий Шейко <sheydim@gmail.com>
2023-01-24 19:40:34 +0300

    Fix import

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit/dist/configureStore';
+import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 
 export const store = configureStore({
