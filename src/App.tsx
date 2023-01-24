0845f5edaafdfba356c2d951cdd07fda67b081b4
Дмитрий Шейко <sheydim@gmail.com>
2023-01-24 15:00:11 +0300

    Set up application routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 export const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+  );
+};
