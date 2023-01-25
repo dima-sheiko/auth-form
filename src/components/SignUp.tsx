3d2179084c710fe1ef34cc698c2cd6e977253711
Дмитрий Шейко <sheydim@gmail.com>
2023-01-25 17:38:59 +0300

    Create sign up logic

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../hooks/redux';
+import { setUser } from '../store/userSlice';
+import { Form } from './Form';
 
 export const SignUp = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSignUp = (email: string, password: string) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.refreshToken,
+          })
+        );
+        navigate('/');
+      })
+      .catch(console.error);
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div>
+      <Form
+        title='Sign Up'
+        callback={handleSignUp}
+      />
+    </div>
+  );
+};
