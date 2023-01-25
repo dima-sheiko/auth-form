54b5aba6eb3c52dfa8b08fb4c0702a1c017b034c
Дмитрий Шейко <sheydim@gmail.com>
2023-01-25 17:38:34 +0300

    Create sign in logic

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,35 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../hooks/redux';
+import { setUser } from '../store/userSlice';
+import { Form } from './Form';
+
+export const SignIn = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = (email: string, password: string) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.refreshToken,
+          })
+        );
+        navigate('/');
+      })
+      .catch(() => alert('Invalid user!'));
+  };
+
+  return (
+    <div>
+      <Form
+        title='Log In'
+        callback={handleLogin}
+      />
+    </div>
+  );
+};
