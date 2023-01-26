f7bc73e23c04bdfddb84ef32566e135a8ad510c4
Дмитрий Шейко <sheydim@gmail.com>
2023-01-26 20:22:29 +0300

    Add conditional info

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import { IFormData } from '../../types/IFormData';
 
 interface FormProps {
@@ -21,37 +22,51 @@ export const Form = ({ title, callback }: FormProps) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register('email', {
-          required: 'This field is required.',
-          pattern: {
-            value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-            message: 'Please enter a valid email address.',
-          },
-        })}
-        type='email'
-        placeholder='Email'
-      />
-      <p>{errors.email?.message}</p>
-      <input
-        {...register('password', {
-          required: 'This field is required.',
-          minLength: {
-            value: 7,
-            message: 'Password length must be greater than 7 characters.',
-          },
-        })}
-        type='password'
-        placeholder='Password'
-      />
-      <p>{errors.password?.message}</p>
-      <button
-        type='submit'
-        disabled={!isValid || isSubmitting}
-      >
-        {title}
-      </button>
-    </form>
+    <div>
+      <h1>{title}</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register('email', {
+            required: 'This field is required.',
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+              message: 'Please enter a valid email address.',
+            },
+          })}
+          type='email'
+          placeholder='Email'
+        />
+        <p>{errors.email?.message}</p>
+        <input
+          {...register('password', {
+            required: 'This field is required.',
+            minLength: {
+              value: 7,
+              message: 'Password length must be greater than 7 characters.',
+            },
+          })}
+          type='password'
+          placeholder='Password'
+        />
+        <p>{errors.password?.message}</p>
+        <button
+          type='submit'
+          disabled={!isValid || isSubmitting}
+        >
+          {title}
+        </button>
+      </form>
+      {title === 'Sign In' ? (
+        <div>
+          <p>Not a member?</p>
+          <Link to='/register'>Register now</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Already have an account?</p>
+          <Link to='/login'>Sign in</Link>
+        </div>
+      )}
+    </div>
   );
 };
