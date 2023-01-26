d0697d9fcd88e6a6002c2b90524cb28a8f50e8fc
Дмитрий Шейко <sheydim@gmail.com>
2023-01-26 12:51:09 +0300

    Change name to index

---
+++ src/components/SignUp/index.tsx
@@ -0,0 +1,35 @@
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../../hooks/redux';
+import { setUser } from '../../store/userSlice';
+import { Form } from '../Form';
+
+export const SignUp = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSignUp = (email: string, password: string) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.refreshToken,
+          })
+        );
+        navigate('/');
+      })
+      .catch(console.error);
+  };
+
+  return (
+    <div>
+      <Form
+        title='Sign Up'
+        callback={handleSignUp}
+      />
+    </div>
+  );
+};
