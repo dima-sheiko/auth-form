d2da8e97e62ee0ca5bfeef755350198b35492be6
Дмитрий Шейко <sheydim@gmail.com>
2023-01-26 12:45:44 +0300

    Implement logout logic

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { getAuth, signOut } from 'firebase/auth';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../hooks/redux';
+import { removeUser } from '../store/userSlice';
+import { useAuth } from '../hooks/useAuth';
 
 export const HomePage = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  const { isAuth, email } = useAuth();
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(removeUser());
+        navigate('/login');
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return isAuth ? (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={handleSignOut}>Log out from {email}</button>
+    </div>
+  ) : (
+    <Navigate to='/login' />
+  );
+};
