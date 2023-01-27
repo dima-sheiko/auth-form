20e0820becff53e6fc9b40fd31bc22740e60f488
Дмитрий Шейко <sheydim@gmail.com>
2023-01-27 15:34:10 +0300

    Style form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { IFormData } from '../../types/IFormData';
+import icon from '../../assets/icon.svg';
+import styles from './Form.module.css';
 
 interface FormProps {
   title: string;
@@ -22,47 +24,62 @@ export const Form = ({ title, callback }: FormProps) => {
   };
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register('email', {
-            required: 'This field is required.',
-            pattern: {
-              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-              message: 'Please enter a valid email address.',
-            },
-          })}
-          type='email'
-          placeholder='Email'
-        />
-        <p>{errors.email?.message}</p>
-        <input
-          {...register('password', {
-            required: 'This field is required.',
-            minLength: {
-              value: 7,
-              message: 'Password length must be greater than 7 characters.',
-            },
-          })}
-          type='password'
-          placeholder='Password'
-        />
-        <p>{errors.password?.message}</p>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>{title}</h1>
+      <img
+        className={styles.user}
+        src={icon}
+        alt='user icon'
+      />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={styles.form}
+      >
+        <div className={styles.input_wrapper}>
+          <input
+            {...register('email', {
+              required: 'This field is required.',
+              pattern: {
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                message: 'Please enter a valid email address.',
+              },
+            })}
+            className={styles.input}
+            type='email'
+            placeholder='Email'
+          />
+          <p className={styles.error}>{errors.email?.message}</p>
+        </div>
+        <div className={styles.input_wrapper}>
+          <input
+            {...register('password', {
+              required: 'This field is required.',
+              minLength: {
+                value: 7,
+                message: 'Password length must be greater than 7 characters.',
+              },
+            })}
+            className={styles.input}
+            type='password'
+            placeholder='Password'
+          />
+          <p className={styles.error}>{errors.password?.message}</p>
+        </div>
         <button
+          className={styles.button}
           type='submit'
-          disabled={!isValid || isSubmitting}
+          disabled={isSubmitting}
         >
           {title}
         </button>
       </form>
       {title === 'Sign In' ? (
-        <div>
-          <p>Not a member?</p>
-          <Link to='/register'>Register now</Link>
+        <div className={styles.info}>
+          <p>Don't have an account?</p>
+          <Link to='/register'>Sign Up</Link>
         </div>
       ) : (
-        <div>
+        <div className={styles.info}>
           <p>Already have an account?</p>
           <Link to='/login'>Sign in</Link>
         </div>
