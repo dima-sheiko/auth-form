8ca84d4b18d5c1fdccc6b042d18730ee72e613ea
Дмитрий Шейко <sheydim@gmail.com>
2023-01-30 14:02:22 +0300

    Add signout logic

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../../hooks/redux';
+import { removeUser } from '../../store/userSlice';
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(removeUser());
+        navigate('/login');
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return <div>Home</div>;
+};
