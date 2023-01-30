a0cb2d8e60f9f83deba222e846599fdabceee347
Дмитрий Шейко <sheydim@gmail.com>
2023-01-30 14:04:27 +0300

    Refactor Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,33 +1,9 @@
-import { getAuth } from 'firebase/auth';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../hooks/redux';
-import { removeUser } from '../store/userSlice';
+import { Navigate } from 'react-router-dom';
+import { Home } from '../components/Home';
 import { useAuth } from '../hooks/useAuth';
 
 export const HomePage = () => {
-  const { isAuth, email } = useAuth();
+  const { isAuth } = useAuth();
 
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  const auth = getAuth();
-
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        dispatch(removeUser());
-        navigate('/login');
-      })
-      .catch((err) => console.log(err));
-  };
-
-  return isAuth ? (
-    <div>
-      <h1>Welcome</h1>
-      <button onClick={handleSignOut}>Log out from {email}</button>
-    </div>
-  ) : (
-    <Navigate to='/login' />
-  );
+  return isAuth ? <Home /> : <Navigate to='/login' />;
 };
