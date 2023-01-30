5fea743505699a410fba8c7b6c2bf19ca016d054
Дмитрий Шейко <sheydim@gmail.com>
2023-01-30 16:08:01 +0300

    Add and style content

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -2,6 +2,7 @@ import { getAuth } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/redux';
 import { removeUser } from '../../store/userSlice';
+import styles from './Content.module.css';
 
 export const Content = () => {
   const dispatch = useAppDispatch();
@@ -19,5 +20,21 @@ export const Content = () => {
       .catch((err) => console.log(err));
   };
 
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Welcome</h1>
+        <iframe
+          src='https://www.youtube.com/embed/dQw4w9WgXcQ'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        />
+        <button
+          className={styles.button}
+          onClick={handleSignOut}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
